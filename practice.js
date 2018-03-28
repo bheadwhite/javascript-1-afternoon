5acaf0cb10913cc81a9709438607a3f582f857c1
Bdub <Bdub@Gordons-iMac.local>
2018-03-27 18:51:04 -0600

    got to problem 9

--- practice.js
+++ practice.js
@@ -31,7 +31,7 @@ function(name) => "Hello, " + name;
 //"apples", "milk", "eggs", "bread"
 
 //Code Here
-
+const groceries = ["apples", "milk", "eggs", "bread"]
 //Write a function called doubleCheck that takes in an array
 //as a parameter.
 
@@ -39,7 +39,12 @@ function(name) => "Hello, " + name;
 //doubleCheck should return the array.
 
 //Code Here
-
+function doubleCheck (arr){
+  if(arr.indexOf("chocolate") === -1){
+    arr.push("chocolate")
+  }
+  return arr
+}
 //////////////////PROBLEM 5////////////////////
 
 //Create an object saved to the variable dog.
@@ -48,21 +53,28 @@ function(name) => "Hello, " + name;
 //and goodBoy (a boolean).
 
 //Code Here
-
+const dog = {
+  name: "billy",
+  color: "lilac",
+  age: 1,
+  goodBoy: true
+}
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
 //Code Here
-
+const devMountainClassPet = dog.name;
 //Add a method to dog called bark.
 //The value of bark should be a function that returns the string "Woof woof".
 
 //Code Here
-
+dog.bark = function(){
+  return "Woof woof";
+}
 //Store the result of invoking the bark method in a variable called ruff.
 
 //Code Here
-
+const ruff = dog.bark()
 //////////////////PROBLEM 6////////////////////
 
 //Write a function called looper that takes in an array. looper should declare
@@ -75,7 +87,15 @@ function(name) => "Hello, " + name;
 //Return mySum.
 
 //Code Here
-
+function looper(arr){
+  let mySum = 0;
+  for(let i =0; i<arr.length; i++){
+    if(arr[i]%2 === 1 || arr[i]>= 100){
+      mySum+=arr[i];
+    }
+  }
+  return mySum;
+}
 //////////////////PROBLEM 7////////////////////
 
 //Given the following function called math
@@ -88,12 +108,14 @@ function math(num1, num2, callback) {
 //returns the result of adding them together.
 
 //Code Here
-
+function add(num1, num2){
+  return num1+num2;
+}
 //Now invoke math, passing in the numbers 3 and 4, and your add function,
 //storing the result in the variable mathSum.
 
 //Code Here
-
+const mathSum = math(3,4, add);
 //////////////////PROBLEM 8////////////////////
 
 //Write a function called invoker that takes in one paramter, a callback function.
@@ -108,7 +130,9 @@ function sampleCallbackTwo() {
 }
 
 //Code Here
-
+function invoker(callback){
+  return callback()
+}
 //////////////////PROBLEM 9////////////////////
 
 let duck = "cute";
@@ -131,16 +155,16 @@ function pond() {
 //as strings.
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let globalScope = ["duck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathroomScope = ["duck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathtubScope = ["duck", "sailorDuck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let pondScope = ["duck", "realDuck"];
 
 //////////////////PROBLEM 10////////////////////
 
